93d5ff4ec6a4836fa891ae46bd2cac86cee9b9cf
prem6309 <135264222+prem6309@users.noreply.github.com>
2023-06-04 04:58:49 +0000

    Changes Made In Code

--- Programs/game.py
+++ Programs/game.py
@@ -1,34 +1,49 @@
-from random import randint
 import random
-from random import randrange
 
 random_color = ["R", "G", "B", "Y", "W", "O"]
 random_list = random.sample(random_color, 6)
+list = random_list[1:5]
 
-print(random_list[1:5])
+print(list)
 
 def Game():
     print("Welcome to Mastermind!", "Attempt to guess the 4 digit code...", "You have 10 attempts to guess the code.")
     print("The code is made up of 4 of the following colors: R, G, B, Y, W, O")
 
-guess = input("Enter your guess: ")
+guess_Count = 0
+guesses = 10
+#guess = input("Enter your guess: ")
+#guessed_colors = []
 
-incorrect_position = 0
-correct_position = 0
+incorrect_Position = 0
+correct_Position = 0
 
-while guess != random_list:
-    if guess == 10:
-        print("You have run out of guesses! The code was: ", random_list[1:5])
-    elif guess == random_list[1:5]:
-        print("You have guessed the code! The code was: ", random_list[1:5])
+for i in range(guesses):
+    guess_Count += 1
+    guess = input("Enter your guess: ").upper().split()
+
+    if guess == list:
+        print("You win!")
+        break
+    elif guess == list[1]:
+        guess = input("Enter your guess: ").upper().split()
+        correct_Position += 1
+        print(f"Correct Position: {correct_Position}, Incorrect Position: {incorrect_Position}" )
+    elif guess == list[2]:
+        guess = input("Enter your guess: ").upper().split()
+        correct_Position += 1
+        print(f"Correct Position: {correct_Position}, Incorrect Position: {incorrect_Position}" )
+    elif guess == list[3]:
+        guess = input("Enter your guess: ").upper().split()
+        correct_Position += 1
+        print(f"Correct Position: {correct_Position}, Incorrect Position: {incorrect_Position}" )
     else:
         print("You have guessed incorrectly! Please try again.")
-        guess = input("Enter your guess: ")
+        print(f"Correct Position: {correct_Position}, Incorrect Position: {incorrect_Position}" )
+        guess = input("Enter your guess: ").upper().split()
+    incorrect_Position += 1
 
-while guess != incorrect_position:
-    if incorrect_position == random_list[1:5]:
-        incorrect_position += 1
-    else:
-        correct_position += 1
+if guess_Count == guesses:  # Check if the maximum number of attempts is reached
+    print("Game over. You have run out of attempts.")
 
 Game()
